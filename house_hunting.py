90c411935ae8959d93571dc6e3b1b2c6c24bb398
Albany <albany.prince@gmail.com>
2021-02-16 12:38:16 -0500

    free britney. we got something

--- house_hunting.py
+++ house_hunting.py
@@ -13,20 +13,20 @@ annual_salary = int(input("What is your annual salary? "))
 portion_saved = float(input("How much per month will you save? Write as a decimal. ") or "0.10 ") 
 portion_down_pymt = float(input("What portion is your down payment ") or "0.25 ")
 r = float(input("What is the rate of return ") or "0.04 ")    
-current_savings = 0
 down_pymt = 0
 
 def months():
     months = 0
+    current_savings = 0
     monthly_salary = annual_salary/12
     monthly_amount_saved = monthly_salary*portion_saved
     interest_earned = current_savings*r/12
     down_pymt = total_cost*portion_down_pymt
     
-while current_savings < down_pymt:
-    current_savings = monthly_amount_saved + interest_earned
-    months += 1
+    while current_savings < down_pymt:
+        current_savings += monthly_amount_saved + interest_earned
+        months += 1
 
-    print("You will need 'months' to buy this home")    
+    print (f"You will need {months} month(s) to buy this home")    
 
 months()
